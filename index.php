067ef9537d9f59003e7afab4924b8765a00e9101
Rai Siqueira <rai93siqueira@gmail.com>
2015-04-24 11:24:30 +0000

    Landing Page Baixe Shows

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="http://static.baixeshows.com.br/img/favicon.ico" type="image/ico"/>
+		<title>Baixe Shows</title>
+		<!-- SEO Tags -->
+		<meta name="description" content="Uma nova maneira de ouvir e Baixar Música">
+		<meta name="keywords" content="baixe shows, baixeshows, baixe shows .com . br, shows de forró, download forró, bandas, banda de forró, gustavo moura e Rafael, gustavo moura, rafael, aviões, aviões do forró, bonde do forró, shows ao vivo, encantus, cavaleiros do forró, cavalo de pau, caviar com rapadura, cheiro de menina, desejo de menina, ferro na boneca, forró de farra, forró do movimento, forró do muído, forró dos plays, forró de salto, forró das minas, forró pegado, forró real, forró torpedo,banda torpedo, forró da pegação, forró na farra, gabriel diniz, garota safada, gaviões do forró, gatinha manhosa, limão com mel, magníficos, mastruz com leite, mel com terra, moleca 100 vergonha, moleca sem vergonha, mala 100 alça, mala sem alça, saia rodada, wesley safadão, wesley oliveira, gd magic, gd 360, vibe surreal, setanejo">
+		<link rel="canonical" href="https://baixeshows.com.br"/>
+		<link rel="publisher" href="https://plus.google.com/+RaiSiqueira"/>
+		<meta property="og:locale" content="en_US"/>
+		<meta property="og:type" content="website"/>
+		<meta property="og:title" content="Baixe Shows - Uma nova Maneira de baixar Música"/>
+		<meta property="og:description" content="Uma Nova Maneira de Baixar Música"/>
+		<meta property="og:url" content="https://baixeshows.com.br"/>
+		<meta property="og:site_name" content="baixeshows"/>
+		<meta property="article:publisher" content="http://facebook.com/BaixeShows"/>
+		<meta property="fb:admins" content="100001023664040"/>
+		<meta property="og:image" content="http://static.baixeshows.com.br/img/fb.png"/>
+
+
+		<!-- Site CSS -->
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400' rel='stylesheet' type='text/css'>
+		<link href="css/animate.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+	<!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#sobre">Sobre</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contato">Contato</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+        <!-- Intro bg -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                    <div class="brand-heading">
+                        <img class="img-responsive img-center" src="img/logo-bs.png" height="167" width="315" alt="">
+                    </div>
+                        <p class="intro-text">Uma nova maneira de<br> Baixar e ouvir Música!</p>
+                        <a href="#sobre" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+        <!-- Seção Sobre -->
+    <section id="sobre" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Sobre o Baixe Shows</h2>
+                <p>O Baixe Shows é um serviço para compartilhamento de Músicas, e Vídeos e Notícias relacionados a Bandas e Artistas.</p>
+                <p>O Serviço conta com os melhores servidores, para proporcionar uma melhor experiência para o usuário na hora do Download do Show, da Playlist ou do Vídeo. Todo site está sendo desenvolvido nos melhores padrões, para passar a melhor experiência, tanto no Mobile, quanto no Computador, não importa de onde você esteja acessando o site, entregaremos o melhor conteúdo para seus olhos e seus ouvidos ;-) .</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Seção contato -->
+    <section id="contato" class="content-section text-center">
+        <div class="secao-email">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Cadastre-se</h2>
+                    <p>Digite Seu E-mail, e fique por dentro das Novidades que estamos aprontando.... :D</p>
+                    <form action="//baixeshows.us6.list-manage.com/subscribe/post?u=c6ba251181ace30e9edd9315b&amp;id=9911a832a5" method="post" class="subscribe-form" id="sub_form">
+                    <input type="email" class="form-email" name="email" placeholder="Digite aqui seu email">
+                    <button type="submit" class="btn-enviar">CADASTRAR</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+        <!-- rodape -->
+    <footer>
+        <div class="container text-center">
+            <p>Copyright &copy; Baixe Shows <?php echo date('Y') ?></p>
+        </div>
+    </footer>
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<script src="//cdn.jsdelivr.net/jquery.easing/1.3/jquery.easing.1.3.min.js"></script>
+		<script src="js/site.js" type="text/javascript"></script>
+	</body>
+</html>
---
+++ js/site.js
@@ -0,0 +1,25 @@
+
+// colapse menu depois do scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// page scroll
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// fecha menu responsivo no clique
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
